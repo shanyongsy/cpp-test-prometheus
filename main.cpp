aa73e2c5fb38e69c51e0690587d8aa7ba5aec155
Yong Shan <shanyong@kingsoft.com>
2023-09-25 14:01:44 +0800

    add:[prometheus] 调整 Prometheus 的引用方式

--- main.cpp
+++ main.cpp
@@ -1,20 +1,10 @@
-#include "prometheus/counter.h"
-#include "prometheus/exposer.h"
-#include "prometheus/registry.h"
-
-#include <array>
-#include <chrono>
-#include <cstdlib>
-#include <memory>
-#include <string>
 #include <thread>
-
 #include <iostream>
 #include <unistd.h> // 包含 getopt 函数的头文件
 
-int main(int argc, char* argv[]) {
-    using namespace prometheus;
+#include "PrometheusMgr.h"
 
+int main(int argc, char* argv[]) {
     int port = 8080; // 默认端口号
 
     int opt;
@@ -30,59 +20,19 @@ int main(int argc, char* argv[]) {
     }
     std::cout << "Port: " << port << std::endl;
 
-    // create an http server running on port 8080
-    std::string addr = "127.0.0.1:" + std::to_string(port);
-    Exposer exposer{addr};
-
-    // create a metrics registry
-    // @note it's the users responsibility to keep the object alive
-    auto registry = std::make_shared<Registry>();
-
-    // add a new counter family to the registry (families combine values with the
-    // same name, but distinct label dimensions)
-    //
-    // @note please follow the metric-naming best-practices:
-    // https://prometheus.io/docs/practices/naming/
-    auto& packet_counter = BuildCounter()
-                                .Name("observed_packets_total")
-                                .Help("Number of observed packets")
-                                .Register(*registry);
-
-    // add and remember dimensional data, incrementing those is very cheap
-    auto& tcp_rx_counter =
-        packet_counter.Add({{"protocol", "tcp"}, {"direction", "rx"}});
-    auto& tcp_tx_counter =
-        packet_counter.Add({{"protocol", "tcp"}, {"direction", "tx"}});
-    auto& udp_rx_counter =
-        packet_counter.Add({{"protocol", "udp"}, {"direction", "rx"}});
-    auto& udp_tx_counter =
-        packet_counter.Add({{"protocol", "udp"}, {"direction", "tx"}});
-
-    // add a counter whose dimensional data is not known at compile time
-    // nevertheless dimensional values should only occur in low cardinality:
-    // https://prometheus.io/docs/practices/naming/#labels
-    auto& http_requests_counter = BuildCounter()
-                                    .Name("http_requests_total")
-                                    .Help("Number of HTTP requests")
-                                    .Register(*registry);
-
-    // ask the exposer to scrape the registry on incoming HTTP requests
-    exposer.RegisterCollectable(registry);
+    PrometheusMgr::GetInstace().Init("瑶池", "GameServer_1", port);
 
     for (;;) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
         const auto random_value = std::rand();
 
-        if (random_value & 1) tcp_rx_counter.Increment();
-        if (random_value & 2) tcp_tx_counter.Increment();
-        if (random_value & 4) udp_rx_counter.Increment();
-        if (random_value & 8) udp_tx_counter.Increment();
+        PrometheusMgr::GetInstace().UpdateOnlineUser(random_value % 1000);
 
-        const std::array<std::string, 4> methods = {"GET", "PUT", "POST", "HEAD"};
-        auto method = methods.at(random_value % methods.size());
-        // dynamically calling Family<T>.Add() works but is slow and should be
-        // avoided
-        http_requests_counter.Add({{"method", method}}).Increment();
+        const int32_t msgPrimaryID = random_value % 4;
+        const int32_t msgSecondaryID = random_value % 4;
+        const int32_t msgSize = random_value % 10000;
+        PrometheusMgr::GetInstace().IncrMsgInfo(true, msgPrimaryID, msgSecondaryID, msgSize);
     }
+
     return 0;
 }
